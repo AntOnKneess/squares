5123e3a7f78175e90af3b84bebf5965923871e07
Conker923four <31983474+Conker923four@users.noreply.github.com>
2021-06-27 17:55:29 +1000

    eref

--- player.py
+++ player.py
@@ -11,12 +11,13 @@ class Player(pygame.sprite.Sprite):
         self.surf = pygame.Surface((self.width, self.height))
         self.surf.fill((50, 50, 50))
         self.rect = self.surf.get_rect()
-        self.rectBottom = pygame.Rect(x,y+(self.height / 2),25,3)
-        self.rectLeft = pygame.Rect(x-(self.width/2),y,3,25)
+        self.rectBottom = pygame.Rect(x,y+(self.height / 2),30,5)
+        self.rectLeft = pygame.Rect(x-(self.width/2),y,5,30)
         self.image = self.surf
         self.grav = 0
         self.jump = 0
         self.isground = False
+        self.LeftWall = False
 
         
     def UpdateRects(self, cX, cY):
@@ -36,28 +37,54 @@ class Player(pygame.sprite.Sprite):
         if(key[pygame.K_s]):
             self.y += 1 * self.speed
         if(key[pygame.K_a]):
-            self.x -= 1 * self.speed
+            if(self.LeftWall == False):
+                self.x -= 1 * self.speed
         if(key[pygame.K_d]):
             self.x += 1 * self.speed
 
+        self.LeftWall = False
+        self.isground = False
+        
         for i in collisions:
+
+            
 
             if(self.rectBottom.colliderect(i.rect)):
                 while(self.rectBottom.colliderect(i.rect)):
-                    self.y -= 1
-                    self.rect.center = (self.x - cX, self.y - cY)
-                    self.rectBottom.center = (self.x - cX, self.y +(self.height / 2) - cY)
+                    self.y -= 0.25
+                    self.UpdateRects(cX, cY)
 
+            if(self.rectLeft.colliderect(i.rect)):
+                self.jump = 0
+                self.grav = 0
+                while(self.rectLeft.colliderect(i.rect)):
+                    self.x += 0.25
+                    self.UpdateRects(cX, cY)
 
             self.rectBottom.center = (self.x - cX, self.y +(self.height / 2) + 1 - cY)
             if(self.rectBottom.colliderect(i.rect)):
                 self.grav = 0
                 self.isground = True
-            else:
-                self.isground = False
             self.UpdateRects(cX, cY)
+            
+            self.rectLeft.center = (self.x - cX - (self.width/2) - 1, self.y - cY)
+            if(self.rectLeft.colliderect(i.rect)):
+                self.LeftWall = True
+            self.UpdateRects(cX, cY)
+            
+
+                
+            
+            
         if(self.isground != True):
-            self.grav += 0.75
+            if(self.LeftWall != True):
+                self.grav += 0.75
+            else:
+                self.grav += 0.1
+            
+            
+        
+            
         if(self.jump > 0):
             self.grav -= self.jump
             self.jump -= 1
